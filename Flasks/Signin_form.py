a10b8d421a005ffe7ba6dfca71c5c30875c7a76c
Payal1899 <payallute9@gmail.com>
2026-04-02 18:08:09 +0000

    Updated

--- Flasks/Signin_form.py
+++ Flasks/Signin_form.py
@@ -1,14 +1,26 @@
+
+#create Flask api with routes to pages, use render template to goto html Form page
+#create a html file with post method and HTML form with submit button, define action for submit to come to api page
+#Get data from user
+#create a db user in mongo db, in network access give your public ip address so that you may connect to db with this machine
+#Goto cluster and click connect>connect to your application>>do basic settings and done
+#you will receive URI paste it in .env(separately created file) to secure creds
+#created requirements.txt for all the required import pckgs
+#run command pip install -r requirements.txt to install all of them
+
 from flask import Flask, request, render_template
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+import pymongo
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+load_dotenv() #calling function to load uri stored in .env file
+MONGO_URI = os.getenv('uri') # same variable which is passed in .env file
+client = pymongo.MongoClient(MONGO_URI)#created a client by providing the URI
 
-uri = "mongodb+srv://dummy:1234@project.9kmda5y.mongodb.net/?appName=Project"
-#dummy:<db_password> : provide exact pswd
+db = client.test#created DB named 'test' in cluster name mentioned in URI
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+collection = db['flask-tutorial']#created collection named 'flask-tutorial'
 
 # Send a ping to confirm a successful connection
 try:
@@ -17,7 +29,7 @@ try:
 except Exception as e:
     print(e)
 
- #request for reuest function
+
 app= Flask(__name__) #creating flask 
 @app.route('/')
 def first_fun():
@@ -30,7 +42,11 @@ def sec_fun():
 
     #another way to print data on page
     #dict1=dict(request.form)
-    #return dict1
-
+    #return 
+    
+    #inserting into DB
+    dict1=dict(request.form)
+    collection.insert_one(dict1) #inserting data into our DB using collection
+    return 'Data inserted successfully'
 if __name__ == '__main__':
     app.run(debug=True)
