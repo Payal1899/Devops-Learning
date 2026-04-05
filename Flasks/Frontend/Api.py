e5811045d448b710edf3bcbb9703335539f6e3a4
Payal1899 <payallute9@gmail.com>
2026-04-05 18:32:07 +0000

    Updated

--- Flasks/Frontend/Api.py
+++ Flasks/Frontend/Api.py
@@ -1,26 +1,23 @@
-
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import requests  #this is python lib to connect https requests
 
 Backend_URL='http://0.0.0.0:8000'
 app= Flask(__name__) #creating flask 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def first_fun():
+    if request.method=='POST':
+        dict1=dict(request.form)
+        if not dict1.get('name') or not dict1.get('email'):
+            return render_template('index.html', error="All fields required")
+                
+        else:
+            requests.post(Backend_URL+'/submit', json=dict1) 
+            return redirect(url_for('sec_fun'))
+    
     return render_template('index.html')
-
-@app.route('/submit', methods=['POST'])
+@app.route('/Submit')
 def sec_fun():
-    dict1=dict(request.form)
-    print(dict1)
-    requests.post(Backend_URL+'/submit', json=dict1) 
-    if not dict1.get('name') or not dict1.get('email'):
-        #return render_template('index.html', error="All fields required")
-        render_template('index.html', error="All fields required")
-        return redirect('/')
-        
-    else:
-        collection.insert_one(dict1) #inserting data into our DB using collection
-        return 'Data inserted successfully'
+    return 'Data submitted successfully'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=6000, debug=True)  #running frontend and backend on different ports
