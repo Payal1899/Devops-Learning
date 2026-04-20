78d6889d66d84f49c83e4f6daa3bf8a27eb64011
Payal1899 <payallute9@gmail.com>
2026-04-20 17:40:55 +0000

    All the files updated for new app

---
+++ Docker/Backend/Business.py
@@ -0,0 +1,5 @@
+def get_data():
+    with open('names.txt') as f:
+        name= f.read()
+        name=name.split()
+        return name
---
+++ Docker/Frontend/app.js
@@ -0,0 +1,15 @@
+//exress app that serves html files
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.listen(3000, function() {
+  console.log('Ares listening on port 3000!');
+});
